7a1b1173c219e7296223d2f3c3e435925c783fd5
Aom_exe <sonsin201053@gmail.com>
2026-05-30 21:18:30 +0700

    อัปเดตโค้ดเพิ่มปุ่มรับชื่อ

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
 import streamlit as st
-import time
 
-st.set_page_config(page_title="เกมลับ", page_icon="👻")
-st.markdown("""
-    <style>
-    .stApp { background-color: #000000; color: #ffffff; }
-    </style>
-    """, unsafe_allow_html=True)
+st.stitle("ยินดีต้อนรับสู่เว็ปปัญญาอ่อน")
 
-st.title("มาวัดใจกันหน่อย?")
-st.write("ลองกดปุ่มด้านล่างดูสิ... ถ้ากล้าพอ")
+username = st.test.input("กรอกชื่อก่อนนนนนน")
 
-if st.button("กดตรงนี้"):
-    with st.spinner('กำลังโหลดข้อมูลลับ...'):
-        time.sleep(3)
-    st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJ4eG14bHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4eHh4JmVwPXYxX2ludGVybmFsX2dpZl9ieV9pZCZjdD1n/12u87AYOFp8yWk/giphy.gif")
-    st.subheader("555555555 ตกใจเหรอ?")
-    st.balloons()
+if st.button("งั้นมาเริ่มกันเลย"):
+    if username:
+        st.write(f"สวัสดีค้าบบ {username} เซอร์ไพร์อยู่ข้างหน้า")
+        st.ballons()
+    else:
+        st.error("รีบไปไหนกรอกชื่อก่อนเฮ้ย")    
